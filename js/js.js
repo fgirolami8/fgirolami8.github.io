85a6d1ac70ab94713249ef0b24b9b610baef847e
fgirolami8 <90563389+fgirolami8@users.noreply.github.com>
2022-11-26 13:55:09 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -60,60 +60,105 @@ function showNav() {
 let lftBtn_Carrousel_ARR = document.querySelectorAll('.atras');
 let rgtBtn_Carrousel_ARR = document.querySelectorAll('.adelante');
 
+//para cada carrousel si no se mantiene el valor de giro para todos
+let counterTranslation=[0, 0, 0];//los asocio segun clase de carrousel.parentNode (mejor con obj)
+let counterRotation=[0, 0, 0];
+let actualCarrousel, xMove, xLimit;
+
 lftBtn_Carrousel_ARR.forEach(e => {
     e.addEventListener('click', function(event){
+
         let game = event.currentTarget;
         let carrousel = game.parentNode.querySelectorAll('.carrousel')[0];
-        console.log(carrousel);
-        let XYmove = 790;//lanzamiento-carrousel
-        if(carrousel.classList.contains("carrousel-home"))
-            (window.getComputedStyle(game).getPropertyValue('height') == "300px") ? XYmove = 250 : XYmove = 130;//si es mob-first se mueve menos el carrousel
-        if(carrousel.style.left=="")
-            carrousel.style.left=0;
-        carrousel.style.left= parseFloat(carrousel.style.left) + XYmove +"px";
-        //si se pasa q vuelva a left 0 
-        if(parseFloat(carrousel.style.left)>=0){
-            carrousel.style.left= 0 + "px";
+        let carrChildren = carrousel.children;
+
+        if(carrousel.parentNode.classList.contains("sugerencias")) actualCarrousel = 0;
+        else if (carrousel.parentNode.classList.contains("juegosArmas")) actualCarrousel = 1;
+        else actualCarrousel = 2;
+
+        xLimit = 0;
+        if (window.getComputedStyle(game).getPropertyValue('height') == "300px") xMove = 250; 
+        else xMove = 130;
+        if (counterTranslation[actualCarrousel]>= 0) return;
+
+        counterRotation[actualCarrousel]+=360;
+        counterTranslation[actualCarrousel]+= xMove;//si es mob-first se mueve menos el carrousel;
+
+        for (let i = 0; i < carrChildren.length; i++) {
+            carrChildren[i].style.transform="translate("+counterTranslation[actualCarrousel]+"px) rotate("+counterRotation[actualCarrousel]+"deg)"
         }
     });
 });
+
 
 rgtBtn_Carrousel_ARR.forEach(e => {
     e.addEventListener('click', function(event){
         let game = event.currentTarget;
         let carrousel = game.parentNode.querySelectorAll('.carrousel')[0];
-        console.log(carrousel);
-        let carrChildren;
-        let rotType;
-        if(carrousel.classList.contains("carrousel-home")){
-        rotType = 'rotateCardR';
-        carrChildren = carrousel.children;
-        for (let i = 0; i < carrChildren.length; i++) {
-            carrChildren[i].classList.toggle(rotType);
-            console.log(carrChildren[i].classList);
+        // console.log(carrousel.parentNode);
+        let carrChildren = carrousel.children;
+        //compruebo en q carrousel estoy
+        if(carrousel.parentNode.classList.contains("sugerencias"))
+            actualCarrousel = 0;
+        else if (carrousel.parentNode.classList.contains("juegosArmas"))
+            actualCarrousel = 1;
+        else
+            actualCarrousel = 2;
+        //a cada card la roto y translado con una clase
+        //no pude con clase (add y remove de una clase con transform) xq se deshacia la rotacion previa
+
+        if (window.getComputedStyle(game).getPropertyValue('height') == "300px"){
+            xMove = 250; 
+            xLimit = -750;
+        }else{
+            xMove = 130;
+            xLimit = -520;
         }
-    }
-        let XYmove = 750;        
-        if(carrousel.classList.contains("carrousel-home"))//DISTINTO PARA CARROUSEL DE LANZAMIENTO Y HOME
-        (window.getComputedStyle(game).getPropertyValue('height') == "300px") ? XYmove = 250 : XYmove = 130;
-           
-        if(carrousel.style.left=="")
-            carrousel.style.left=0;
+        if (counterTranslation[actualCarrousel]<= xLimit) return;
 
-        if((parseFloat(carrousel.style.left)<=-750 && XYmove==250) || (parseFloat(carrousel.style.left)<=-520 && XYmove==130) || (parseFloat(carrousel.style.left)<=-1500 && XYmove==750))
-            return;
-        carrousel.style.left = parseFloat(carrousel.style.left) - XYmove + "px";
+        counterRotation[actualCarrousel]-=360;
+        counterTranslation[actualCarrousel]-= xMove;//si es mob-first se mueve menos el carrousel;
 
-        if(carrousel.classList.contains("carrousel-home")) flipCards(carrChildren, rotType)
-    });
+        for (let i = 0; i < carrChildren.length; i++) {
+            carrChildren[i].style.transform="translate("+counterTranslation[actualCarrousel]+"px) rotate("+counterRotation[actualCarrousel]+"deg)"
+            // carrChildren[i].classList.remove("moveCardAnimated");
+        }
+    })
 });
 
-function flipCards(cards, rotType) {
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].classList.add(rotType);
-    }
+// rgtBtn_Carrousel_ARR.forEach(e => {
+//     e.addEventListener('click', function(event){
+//         let game = event.currentTarget;
+//         let carrousel = game.parentNode.querySelectorAll('.carrousel')[0];
+//         console.log(carrousel);
+//         let carrChildren;
+//         let rotType;
+//         if(carrousel.classList.contains("carrousel-home")){
+//         rotType = 'rotateCardR';
+//         carrChildren = carrousel.children;
+//         for (let i = 0; i < carrChildren.length; i++) {
+//             carrChildren[i].classList.toggle(rotType);
+//             console.log(carrChildren[i].classList);
+//         }
+//     }
+//         let XYmove = 750;        
+//         if(carrousel.classList.contains("carrousel-home"))//DISTINTO PARA CARROUSEL DE LANZAMIENTO Y HOME
+//         (window.getComputedStyle(game).getPropertyValue('height') == "300px") ? XYmove = 250 : XYmove = 130;
+           
+//         if(carrousel.style.left=="")
+//             carrousel.style.left=0;
+
+//         if((parseFloat(carrousel.style.left)<=-750 && XYmove==250) || (parseFloat(carrousel.style.left)<=-520 && XYmove==130) || (parseFloat(carrousel.style.left)<=-1500 && XYmove==750))
+//             return;
+//         carrousel.style.left = parseFloat(carrousel.style.left) - XYmove + "px";
+
+//         if(carrousel.classList.contains("carrousel-home")) flipCards(carrChildren, rotType)
+//     });
+// });
+
+
     
-}
+// }
 
 
 
--- js/lanzamiento.js
+++ js/lanzamiento.js
@@ -1,3 +1,26 @@
+//  DESVANECER H1
+
+//cuando se craga pag o el scroll esta en el hero aparece titulo
+
+let h1 = document.querySelector(".heroH1");
+
+document.addEventListener("DOMContentLoaded", function(){
+    h1.classList.add("show-hero-h1");
+})
+window.addEventListener("scroll", show_h1);
+//se muestra con scroll menor a 500
+function show_h1(){
+    if(window.scrollY<350){
+        if(!h1.classList.contains("show-hero-h1")){
+            h1.classList.add("show-hero-h1");
+        }
+    }
+    else{
+        if(h1.classList.contains("show-hero-h1")){
+            h1.classList.remove("show-hero-h1");
+        }
+    }
+}
 
 // CARD DE LANZAMIENTO ANIMADA----------------
 window.addEventListener("scroll", moverCards);
@@ -11,3 +34,64 @@ function moverCards(){
         }
     }
 }
+
+// carrousel
+
+//botones de lft
+let lft = document.querySelectorAll('.lft');
+let rgt = document.querySelectorAll('.rgt');
+
+
+lft.forEach(e => {
+    e.addEventListener('click', function(event){  
+        let game = event.currentTarget;
+        let carrousel = game.parentNode.querySelectorAll('.carrousel')[0];
+        move(750, parseFloat(carrousel.style.left)>=0, carrousel);
+    })
+})
+
+rgt.forEach(e => {
+    e.addEventListener('click', function(event){
+        let game = event.currentTarget;
+        let carrousel = game.parentNode.querySelectorAll('.carrousel')[0];
+        move(-750, parseFloat(carrousel.style.left)<=-1500, carrousel);
+    })
+})
+function move(move, limit, carrousel){
+    if(carrousel.style.left=="")
+        carrousel.style.left=0;
+    if (limit)
+        return;
+    carrousel.style.left = parseFloat(carrousel.style.left) + move + "px";
+}
+
+
+// descripcion
+
+
+window.addEventListener("scroll", function(){
+    let descText1 = document.querySelector(".descText1");
+    let descText2 = document.querySelector(".descText2");
+    let descImg = document.querySelector(".img-desc1");
+    if(window.scrollY>1900 && window.scrollY<2250){
+        descImg.src = "css/img/desc1.jpg";
+        if(!descText2.classList.contains("ocultarDesc"))
+            descText2.classList.add("ocultarDesc");
+        if(descText1.classList.contains("ocultarDesc"))
+            descText1.classList.remove("ocultarDesc");
+
+    }
+    else if(window.scrollY>2250 && window.scrollY<2650){
+        descImg.src = "css/img/desc2.jpg";
+        
+        if(!descText1.classList.contains("ocultarDesc"))
+            descText1.classList.add("ocultarDesc");
+        if(descText2.classList.contains("ocultarDesc"))
+            descText2.classList.remove("ocultarDesc");
+    }else{
+        if(!descText2.classList.contains("ocultarDesc"))
+            descText2.classList.add("ocultarDesc");
+        if(!descText1.classList.contains("ocultarDesc"))
+            descText1.classList.add("ocultarDesc");
+    }
+});
