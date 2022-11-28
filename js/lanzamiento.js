139ee61b7c5a2c7dd92daaf1e2d3e396a6901157
fgirolami8 <90563389+fgirolami8@users.noreply.github.com>
2022-11-28 10:42:48 -0300

    Add files via upload

--- js/lanzamiento.js
+++ js/lanzamiento.js
@@ -42,11 +42,13 @@ let lft = document.querySelectorAll('.lft');
 let rgt = document.querySelectorAll('.rgt');
 
 
+let carrousel = document.querySelectorAll('.carrousel')[0];
+carrousel.style.left=0;
+
 lft.forEach(e => {
     e.addEventListener('click', function(event){  
-        let game = event.currentTarget;
-        let carrousel = game.parentNode.querySelectorAll('.carrousel')[0];
-        move(750, parseFloat(carrousel.style.left)>=0, carrousel);
+        // let game = event.currentTarget;
+        move(750, parseFloat(carrousel.style.left)>=0);
     })
 })
 
@@ -54,12 +56,11 @@ rgt.forEach(e => {
     e.addEventListener('click', function(event){
         let game = event.currentTarget;
         let carrousel = game.parentNode.querySelectorAll('.carrousel')[0];
-        move(-750, parseFloat(carrousel.style.left)<=-1500, carrousel);
+        move(-750, parseFloat(carrousel.style.left)<=-1500);
     })
 })
-function move(move, limit, carrousel){
-    if(carrousel.style.left=="")
-        carrousel.style.left=0;
+function move(move, limit){
+
     if (limit)
         return;
     carrousel.style.left = parseFloat(carrousel.style.left) + move + "px";
@@ -95,3 +96,37 @@ window.addEventListener("scroll", function(){
             descText1.classList.add("ocultarDesc");
     }
 });
+
+//ACERCAMIENTO PERSONAJES  A TITULO
+
+let lastScroll=0;
+window.addEventListener("scroll", acercar);
+let personajesTranslation = 0;
+function acercar() {
+    let cardPersonaje = document.querySelector(".hero-personajes-carrousel");
+    // cuándo suben presonajes
+    if((window.scrollY>800 && window.scrollY<1600)){
+
+    if(lastScroll<window.scrollY){
+        personajesTranslation -= 15;
+        // alert(window.scrollY)
+       
+    }
+    //cuándo bajan presonajes
+    else{
+        //relativo a scroll asi es acorde al scrool final (si le pongo valor fijo y hago scroll grande no queda acercamiento bueno) 
+        personajesTranslation += 15;
+        // bottom limit
+        if(personajesTranslation>=0)
+            personajesTranslation=0;
+    }
+}
+    if(personajesTranslation<=-500 || window.scrollY>1250)
+            personajesTranslation = -500;
+    if(window.scrollY<700)
+        personajesTranslation = 0;
+
+    //para saber si es scroll para arria o abajo
+    lastScroll = window.scrollY;
+    cardPersonaje.style.transform = "translateY(" + personajesTranslation + "px)";
+}
